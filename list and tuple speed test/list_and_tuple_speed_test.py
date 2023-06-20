8bfe5afdf0ae967660cfa1347fba22155fc60abc
st1ko-gs <antonluomala2008@gmail.com>
2023-06-20 07:38:51 +0300

    Add project files.

---
+++ list and tuple speed test/list_and_tuple_speed_test.py
@@ -0,0 +1,14 @@
+
+
+
+# tuples is much faster, so to prove it we are going to do a test 
+
+#list speed test
+import timeit
+
+print(timeit.timeit(stmt='["red", "blue", "green", 5, 7, 12, 18, "dude"]', nummber=10000000))
+
+
+#tuple speed test
+#tuple is faster since its stored in 1 block memory
+print(timeit.timeit(stmt='("red", "blue", "green", 5, 7, 12, 18, "dude")', nummber=10000000))
